b82db66c6e70f6ed9cde71425425c3e20e90260a
Tim Übelhör <t.uebelhoer@irt.rwth-aachen.de>
2019-08-12 16:51:11 +0200

    testing alternating exploit vs explore

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,6 +1,7 @@
 from huskarl.agent import DQN
 from huskarl.policy import Greedy, EpsGreedy
 from huskarl.simulation import Simulation
+import numpy as np
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten
 from validity_2048 import check_valid_2048
@@ -9,25 +10,38 @@ import gym_2048
 import matplotlib.pyplot as plt
 
 
-def create_env(): return gym.make('2048-3x3-v0').unwrapped
+def create_env(): return gym.make('2048-4x4-v0').unwrapped
 
 
 dummy_env = create_env()
 model = Sequential([
     Flatten(input_shape=dummy_env.observation_space.shape),
-    Dense(100, activation='relu'),
-    Dense(100, activation='relu'),
-    Dense(50, activation='relu'),
-    Dense(50, activation='relu'),
-    Dense(20, activation='relu'),
-    Dense(20, activation='relu'),
+    Dense(500, activation='relu'),
+    Dense(500, activation='relu'),
+    Dense(200, activation='relu'),
     Dense(4, activation='linear')
 ])
+
+# Create a policy for each instance with a different distribution for epsilon
+explore_policy = EpsGreedy(0.1, check_valid_2048)
+exploit_policy = Greedy(check_valid_2048)
 # Create Deep Q-Learning Network agent
 agent = DQN(model, actions=dummy_env.action_space.n, gamma=0.99,
-            batch_size=128, nsteps=2, enable_double_dqn=True,
+            batch_size=64, nsteps=2, enable_double_dqn=True,
             enable_dueling_network=True, target_update=10,
-            test_policy=Greedy(check_valid_2048))
+            test_policy=exploit_policy)
+
+
+def plot_rewards_show(episode_rewards, episode_steps, done=False,
+                      title='Rewards'):
+    plt.clf()
+    plt.xlabel('Step')
+    plt.ylabel('Reward')
+    plt.title('DQN rewards')
+    for i, (ed, steps) in enumerate(zip(episode_rewards, episode_steps)):
+        plt.plot(steps, ed, alpha=0.5 if i == 0 else 0.2,
+                 linewidth=2 if i == 0 else 1)
+    plt.show() if done else plt.pause(0.0001)
 
 
 def plot_rewards(episode_rewards, episode_steps, done=False,
@@ -35,32 +49,24 @@ def plot_rewards(episode_rewards, episode_steps, done=False,
     plt.clf()
     plt.xlabel('Step')
     plt.ylabel('Reward')
-    plt.title(title)
-    for ed, steps in zip(episode_rewards, episode_steps):
-        plt.plot(steps, ed)
-    # Pause a bit so that the graph is updated
-    # plt.show() if done else plt.pause(0.0001)
+    plt.title('DQN rewards')
+    for i, (ed, steps) in enumerate(zip(episode_rewards, episode_steps)):
+        plt.plot(steps, ed, alpha=0.5 if i == 0 else 0.2,
+                 linewidth=2 if i == 0 else 1)
     plt.pause(0.0001)
 
 
-# Create simulation, train and then test
+# Create simulation
 print('training the agent')
 sim = Simulation(create_env, agent)
-# explore and reduce epsilon
-n_eps = 5
-eps_max = 0.5
-eps_min = 0.05
-for i in range(n_eps):
-    eps = eps_max - (eps_max-eps_min) / (n_eps - 1) * i
-    agent.policy = EpsGreedy(eps, check_valid_2048)
-
-    def plot_wrapper(episode_rewards, episode_steps, done=False):
-        return plot_rewards(episode_rewards, episode_steps, done,
-                            title=f'Rewards for eps={eps}')
-
-    sim.train(max_steps=10000, visualize=False,
-              plot=plot_wrapper)
-
+# switch between exploitation and exploration
+for i in range(50):
+    agent.policy = exploit_policy
+    sim.train(max_steps=2000, visualize=False, plot=plot_rewards)
+    agent.policy = explore_policy
+    sim.train(max_steps=2000, visualize=False, plot=plot_rewards)
+agent.policy = exploit_policy
+sim.train(max_steps=2000, visualize=False, plot=plot_rewards_show)
 print('testing policy')
 sim.test(max_steps=1000)
 model.save('dqn_agent_v0.tf')
